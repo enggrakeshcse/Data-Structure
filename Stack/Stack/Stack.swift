86014c8ba2ac30b871c7eb3def557e9dfcb18c70
Rakesh <rakesh.kunwar@finoit.co.in>
2019-08-06 13:29:37 +0530

    implements using LinkedList

--- Stack/Stack/Stack.swift
+++ Stack/Stack/Stack.swift
@@ -5,10 +5,47 @@
 //  Created by Rakesh on 06/08/19.
 //  Copyright © 2019 Rakesh. All rights reserved.
 //
-
 import Foundation
+class Node<T: Equatable> {
+    var val: T?
+    var next: Node?
+}
 class Stack<T: Equatable> {
+    var head = Node<T>()
     var StackArray = [T]()
+    // MARK: - using Linked List
+    init() {
+        self.head.next = nil
+    }
+    func pushThroughLinkedList(value: T) {
+        let newNode = Node<T>()
+        newNode.val = value
+        newNode.next = head.next
+        head.next = newNode
+    }
+    func printAllElemetLinkedList() {
+        var current: Node! = head.next
+        if current == nil {
+            print("There is no element in Linked List ")
+        } else {
+            while current.next != nil {
+                print(current.val ?? "")
+                current = current.next
+            }
+            print(current.val ?? "")
+        }
+    }
+    
+    func popThroughLinkedList() {
+        if let head1 = head.next {
+            head = head1
+            print(head.val ?? "")
+            head.val = nil
+        } else {
+            print("underflow")
+        }
+    }
+    // MARK: - using Array
     func push(value: T) {
         self.StackArray.append(value)
     }
@@ -32,5 +69,3 @@ class Stack<T: Equatable> {
         }
     }
 }
-
-
--- Stack/Stack/main.swift
+++ Stack/Stack/main.swift
@@ -10,11 +10,19 @@ import Foundation
 
 // Stack implements using Array
 let stack = Stack<Int>()
-stack.printAllElementsInStack()
-stack.push(value: 2)
-stack.push(value: 3)
-stack.push(value: 4)
-stack.printAllElementsInStack()
-stack.pop()
-stack.printAllElementsInStack()
+//stack.printAllElementsInStack()
+//stack.push(value: 2)
+//stack.push(value: 3)
+//stack.push(value: 4)
+//stack.printAllElementsInStack()
+//stack.pop()
+//stack.printAllElementsInStack()
 
+// Stack Implements using LinkedList
+stack.pushThroughLinkedList(value: 2)
+stack.pushThroughLinkedList(value: 3)
+stack.pushThroughLinkedList(value: 4)
+stack.pushThroughLinkedList(value: 5)
+stack.printAllElemetLinkedList()
+stack.popThroughLinkedList()
+stack.printAllElemetLinkedList()
