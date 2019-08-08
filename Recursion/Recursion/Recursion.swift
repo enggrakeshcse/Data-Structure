a9c7aeda5446ea9ff48337f293fb8d866fe6b2a9
Rakesh <rakesh.kunwar@finoit.co.in>
2019-08-08 18:22:14 +0530

    recursive done

--- Recursion/Recursion/Recursion.swift
+++ Recursion/Recursion/Recursion.swift
@@ -7,6 +7,107 @@
 //
 
 import Foundation
+//Node for linked List
+class Node {
+    var val: Int?
+    var next: Node?
+}
 class Recursive {
-    
+    func newNode(value: Int) -> Node {
+        let newNode = Node()
+        newNode.val = value
+        newNode.next = nil
+        return newNode
+    }
+    // MARK: Question1
+    // Print a pattern without using any loop?
+    // eg: - input = 16
+    // output = 16 11 6 1 -4 1 6 11 16
+    func solution1(input: Int, changes: Int, flag: Bool) {
+        print(changes, separator: " ", terminator: " ")
+        if flag == false && input == changes {
+            return
+        }
+        flag ? changes - 5 > -5 ? solution1(input: input, changes: changes - 5, flag: true): solution1(input: input, changes: changes - 5, flag: false) : solution1(input: input, changes: changes + 5, flag: false)
+    }
+    // MARK: - Question2
+    // implements of Tower of Hanoi?
+    func solution2(input: Int, from: String, to: String, help:String) {
+        if input == 1 {
+            print("Move Disk 1 from rod \(from) to rod \(to)")
+            return
+        }
+        solution2(input: input - 1, from: from, to: to, help: help)
+        print("Move Disk \(input) from rod \(from) to rod \(to)")
+        solution2(input: input - 1, from: from, to: to, help: help)
+    }
+    // MARK: - Question3
+    // Power Set in Lexicographic order?
+    func permuteRec(str: String, n: Int, index: Int, curr: inout String){
+        if index == n {
+            return
+        }
+        print(curr, separator: " ", terminator: " ")
+        for i in index + 1..<n {
+            curr += String(str[str.index(str.startIndex, offsetBy: i)])
+            permuteRec(str: str, n: n, index: i, curr: &curr)
+            let start = curr.index(str.startIndex, offsetBy: 0)
+            let last = curr.index(str.startIndex, offsetBy: curr.count - 1)
+            let range = start..<last
+            curr = curr.substring(with: range)
+        }
+    }
+    func solution3(input: String) {
+        var curr: String = ""
+        var input1 = input
+        input1 = String(input1.sorted())
+        permuteRec(str: input1, n: input1.count, index: -1, curr: &curr)
+    }
+    // MARK: - Question4
+    // Find middle of Singly Linked List using Recursively?
+    var mid: Node?
+    var n: Int?
+    func midPoint(head: Node?) {
+        if head == nil {
+            n = (n ?? 0) / 2
+            return
+        }
+        n = (n ?? 0) + 1
+        midPoint(head: head?.next)
+        n = (n ?? 0) - 1
+        if n == 0 {
+            mid = head
+        }
+    }
+    func solution4(head: Node) {
+        mid = nil
+        n = 1
+        midPoint(head: head)
+        print(mid?.val ?? "")
+    }
+    // MARK: - Question5
+    // Recursive approach for alternating split of Linked List?
+    var a: Node?
+    var b: Node?
+    func moveNode(a:  Node?, b:  Node?) {
+        if a == nil || b == nil {
+            return
+        }
+        if a?.next != nil {
+             a?.next = a?.next?.next
+        }
+        if b?.next != nil {
+             b?.next = b?.next?.next
+        }
+        moveNode(a: a?.next, b: b?.next)
+    }
+    func solution5(head: Node) {
+        if head.next != nil {
+             a = head
+             b = head.next
+            moveNode(a: a, b: b)
+        } else {
+            print("not possible")
+        }
+    }
 }
--- Recursion/Recursion/main.swift
+++ Recursion/Recursion/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let recursion = Recursive()
+// Solution1
+//print("enter your input")
+//let input = Int(readLine()!)!
+//input > 0 ? recursion.solution1(input: input, changes: input, flag: true): print("Enter a positive no")
 
+// Solution2
+//print("enter you input")
+//let input = Int(readLine()!)!
+//input > 0 ? recursion.solution2(input: input, from: "B", to: "A", help: "C") : print("invalid input")
+
+// Solution3
+//print("Enter you input")
+//let input = readLine()!
+//recursion.solution3(input: input)
+
+// Solution3 and Solution4
+let head = recursion.newNode(value: 1)
+head.next = recursion.newNode(value: 2)
+head.next?.next = recursion.newNode(value: 3)
+head.next?.next?.next = recursion.newNode(value: 4)
+head.next?.next?.next?.next = recursion.newNode(value: 5)
+recursion.solution4(head: head)
+recursion.solution5(head: head)
